85d05586e4a6de77a36ee030e63a27c8ff3dcfde
Yuya ONUMA <gion_XY@live.jp>
2017-01-14 04:17:27 +0900

    テンプレート（html/template）を使ってみる

--- chapter1/chat/main.go
+++ chapter1/chat/main.go
@@ -1,23 +1,32 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter , r *http.Request) {
-		w.Write([]byte(`
-			<html>
-				<head>
-					<title>チャット</title>
-				</head>
-				<body>
-					チャットしましょう！
-				</body>
-			</html>
-		`))
+// templ は 1 つのテンプレートを表す
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTP は HTTP リクエストを処理する
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(
+			template.ParseFiles(filepath.Join("templates", t.filename)),
+		)
 	})
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 
 	// Web サーバーを開始
 	if err := http.ListenAndServe(":8080", nil); err != nil {
