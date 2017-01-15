eae9099cd66bba980708352c71b121191802ea8a
Yuya ONUMA <gion_XY@live.jp>
2017-01-15 20:56:50 +0900

    `trace` パッケージ完成

--- chapter1/trace/tracer.go
+++ chapter1/trace/tracer.go
@@ -1,7 +1,24 @@
 package trace
 
+import (
+	"fmt"
+	"io"
+)
+
 // Tracer はコード内での出来事を記録できるオブジェクトを表すインターフェース
 type Tracer interface {
 	Trace(...interface{})
 }
 
+type tracer struct {
+	out io.Writer
+}
+
+func (t *tracer) Trace(a ...interface{}) {
+	t.out.Write([]byte(fmt.Sprint(a...)))
+	t.out.Write([]byte("\n"))
+}
+
+func New(w io.Writer) Tracer {
+	return &tracer{out: w}
+}
--- chapter1/trace/tracer_test.go
+++ chapter1/trace/tracer_test.go
@@ -1,8 +1,18 @@
 package trace
 import (
+	"bytes"
 	"testing"
 )
 
-func TestNow(t *testing.T) {
-	t.Error("まだテストを作成していません")
+func TestNew(t *testing.T) {
+	var buf bytes.Buffer
+	tracer := New(&buf)
+	if tracer == nil {
+		t.Error("New からの返り値が nil です")
+	} else {
+		tracer.Trace("こんにちは，trace パッケージ")
+		if buf.String() != "こんにちは，trace パッケージ\n" {
+			t.Errorf("'%s' という誤った文字列が出力されました", buf.String())
+		}
+	}
 }
