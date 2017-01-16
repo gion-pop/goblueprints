9198076d5dda37e97323c314e0e890fbd829c47f
Yuya ONUMA <gion_XY@live.jp>
2017-01-16 15:43:29 +0900

    `trace` パッケージを利用

--- chapter1/chat/main.go
+++ chapter1/chat/main.go
@@ -5,8 +5,11 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"sync"
+
+	"github.com/gion-xy/goblueprints/chapter1/trace"
 )
 
 // templ は 1 つのテンプレートを表す
@@ -30,6 +33,7 @@ func main() {
 	addr := flag.String("addr", ":8080", "アプリケーションのアドレス")
 	flag.Parse() // 引数を解釈
 	r := NewRoom()
+	r.tracer = trace.New(os.Stdout)
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 	// チャットルームを開始
--- chapter1/chat/room.go
+++ chapter1/chat/room.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gion-xy/goblueprints/chapter1/trace"
 	"github.com/gorilla/websocket"
 )
 
@@ -16,6 +17,8 @@ type room struct {
 	leave chan *client
 	// clients には在室しているすべての client が保持される
 	clients map[*client]bool
+	// tracer はチャットルームで行われた操作のログを受け取る
+	tracer trace.Tracer
 }
 
 func NewRoom() *room {
@@ -33,20 +36,24 @@ func (r *room) run() {
 		case client := <-r.join:
 			// 参加
 			r.clients[client] = true
+			r.tracer.Trace("新しいクライアントが参加")
 		case client := <-r.leave:
 			// 退室
 			delete(r.clients, client)
 			close(client.send)
+			r.tracer.Trace("クライアントが退室")
 		case msg := <-r.forward:
 			// room にいるすべての client にメッセージを転送
 			for client := range r.clients {
 				select {
 				case client.send <- msg:
 					// 送信成功
+					r.tracer.Trace(" -- クライアントに送信")
 				default:
 					// 送信失敗
 					delete(r.clients, client)
 					close(client.send)
+					r.tracer.Trace(" -- 送信に失敗")
 				}
 			}
 		}
