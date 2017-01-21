1f3331ec33595e43af4b1ecbbe6ddf1ea63b8e3d
Yuya ONUMA <gion_XY@live.jp>
2017-01-21 19:31:01 +0900

    ソーシャルログインの実装

--- chapter2/chat/auth.go
+++ chapter2/chat/auth.go
@@ -5,6 +5,9 @@ import (
 	"log"
 	"net/http"
 	"strings"
+
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/objx"
 )
 
 type authHandler struct {
@@ -37,7 +40,44 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 	provider := segs[3]
 	switch action {
 	case "login":
-		log.Println("TODO: ログイン処理", provider)
+		provider, err := gomniauth.Provider(provider)
+		if err != nil {
+			log.Fatalln("認証プロバイダーの取得に失敗")
+		}
+		loginUrl, err := provider.GetBeginAuthURL(nil, nil)
+		if err != nil {
+			log.Fatal("GetBeginAuthURL:", provider, "-", err)
+		}
+		w.Header().Set("Location", loginUrl)
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	case "callback":
+		provider, err := gomniauth.Provider(provider)
+		if err != nil {
+			log.Fatalln("認証プロバイダーの取得に失敗", provider, "-", err)
+		}
+
+		creds, err := provider.CompleteAuth(objx.MustFromURLQuery(r.URL.RawQuery))
+		if err != nil {
+			log.Fatalln("認証を完了できませんでした", provider, "-", err)
+		}
+
+		user, err := provider.GetUser(creds)
+		if err != nil {
+			log.Fatalln("ユーザーの取得に失敗", provider, "-", err)
+		}
+
+		authCookieValue := objx.New(map[string]interface{}{
+			"name": user.Name(),
+		}).MustBase64()
+
+		http.SetCookie(w, &http.Cookie{
+			Name:  "auth",
+			Value: authCookieValue,
+			Path:  "/",
+		})
+
+		w.Header()["Location"] = []string{"/chat"}
+		w.WriteHeader(http.StatusTemporaryRedirect)
 	default:
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprintf(w, "アクション %s には非対応", action)
--- chapter2/chat/main.go
+++ chapter2/chat/main.go
@@ -5,8 +5,14 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"sync"
+
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
 )
 
 // templ は 1 つのテンプレートを表す
@@ -29,6 +35,27 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func main() {
 	addr := flag.String("addr", ":8080", "アプリケーションのアドレス")
 	flag.Parse() // 引数を解釈
+
+	// gomniauth のセットアップ
+	gomniauth.SetSecurityKey(os.Getenv("_CHAT_SECURITY_KEY"))
+	gomniauth.WithProviders(
+		google.New(
+			os.Getenv("_CHAT_GOOGLE_ID"),
+			os.Getenv("_CHAT_GOOGLE_SECRET"),
+			"http://localhost:8080/auth/callback/google",
+		),
+		facebook.New(
+			os.Getenv("_CHAT_FACEBOOK_ID"),
+			os.Getenv("_CHAT_FACEBOOK_SECRET"),
+			"http://localhost:8080/auth/callback/facebook",
+		),
+		github.New(
+			os.Getenv("_CHAT_GITHUB_CK"),
+			os.Getenv("_CHAT_GITHUB_CS"),
+			"http://localhost:8080/auth/callback/github",
+		),
+	)
+
 	r := NewRoom()
 	http.Handle("/", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
