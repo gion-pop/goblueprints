703b8c1ced5a5c93ba10208a12cfa30622c75937
Yuya ONUMA <gion_XY@live.jp>
2017-01-21 19:38:52 +0900

    自分の名前の表示

--- chapter2/chat/main.go
+++ chapter2/chat/main.go
@@ -13,6 +13,7 @@ import (
 	"github.com/stretchr/gomniauth/providers/facebook"
 	"github.com/stretchr/gomniauth/providers/github"
 	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
 )
 
 // templ は 1 つのテンプレートを表す
@@ -29,7 +30,14 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			template.ParseFiles(filepath.Join("templates", t.filename)),
 		)
 	})
-	t.templ.Execute(w, r)
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+
+	t.templ.Execute(w, data)
 }
 
 func main() {
